48279cda2b3833cbcfdec0be9e7a00ff5915a0b3
sergey-komar <kursev81@yandex.ru>
2022-10-17 23:19:16 +0300

    difficul

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
-$(function () {
+window.addEventListener('DOMContentLoaded', () => {
+
 $('.reviews__slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -188,17 +189,27 @@ $('.tachographs-slider__inner-bottom').slick({
       ]
 })
 
-const buttonBtn = document.querySelector('.button-btn');
-const contentHide = document.querySelector('.content-hidden');
-const btnShow = document.querySelector('.content-hide_btn')
-    buttonBtn.addEventListener('click', ()=>{
-        contentHide.classList.toggle('card');
-        buttonBtn.style.display = 'none';
-    })
-    btnShow.addEventListener('click', ()=>{
-        contentHide.classList.toggle('card');
-        buttonBtn.style.display = 'block';
-    })
+
+
+$('.catalog__inner-slider').slick({
+  responsive: [
+    {
+      breakpoint: 580,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
+        arrows:false,
+        dots: true,
+        infinite:false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+})
+
+
 
  const modalBtn = document.querySelectorAll('[data-modal]');
  const modal = document.querySelector('.modal');
@@ -212,8 +223,6 @@ const btnShow = document.querySelector('.content-hide_btn')
         document.body.style.overflow = 'hidden'
     })
 })
-
-
 
 modalClose.addEventListener('click', ()=> {
     modal.classList.remove('show')
@@ -245,4 +254,91 @@ this.classList.toggle('nav-icon--active');
 menu.classList.toggle('nav--active');
 });
 
-});
+
+
+const mobileBtnText = document.querySelectorAll('.page-box__mobile');
+const mobileText = document.querySelector('.page-box__info-mobile');
+const mobileTextfour = document.querySelector('.page-box__info-mobile-four');
+const mobileTextthree = document.querySelector('.page-box__info-mobile-three');
+const mobileTextttwo = document.querySelector('.page-box__info-mobile-two');
+const mobileTextone = document.querySelector('.page-box__info-mobile-one');
+
+  mobileBtnText.forEach(item =>{
+    item.addEventListener('click' ,()=>{
+      mobileText.classList.toggle('show');
+    })
+  })
+
+  mobileBtnText.forEach(item =>{
+    item.addEventListener('click' ,()=>{
+      mobileTextfour.classList.toggle('show');
+    })
+  })
+  mobileBtnText.forEach(item =>{
+    item.addEventListener('click' ,()=>{
+      mobileTextthree.classList.toggle('show');
+    })
+  })
+  mobileBtnText.forEach(item =>{
+    item.addEventListener('click' ,()=>{
+      mobileTextttwo.classList.toggle('show');
+    })
+  })
+  mobileBtnText.forEach(item =>{
+    item.addEventListener('click' ,()=>{
+      mobileTextone.classList.toggle('show');
+    })
+  })
+
+
+  // const productPageOneMobileBtn = document.querySelectorAll('.product-page__mobile');
+  const productpagemobileone = document.querySelector('.product-page__mobile-one');
+  const productpagemobiletwo = document.querySelector('.product-page__mobile-two');
+  const productpagemobilethree = document.querySelector('.product-page__mobile-three');
+  const productPageOne = document.querySelector('.product-page-mobile-one');
+  const productPageTwo = document.querySelector('.product-page-mobile-two');
+  const productPageThree = document.querySelector('.product-page-mobile-three');
+
+  productpagemobileone.addEventListener('click',()=>{
+    productPageOne.classList.toggle('show');
+  });
+  productpagemobiletwo.addEventListener('click',()=>{
+    productPageTwo.classList.toggle('show');
+  });
+  productpagemobilethree.addEventListener('click',()=>{
+    productPageThree.classList.toggle('show');
+  });
+
+  // productPageOneMobileBtn.forEach(item =>{
+  //   item.addEventListener('click' ,()=>{
+  //     productPageOne.classList.toggle('show');
+  //   });
+  // });
+
+  // productPageOneMobileBtn.forEach(item =>{
+  //   item.addEventListener('click' ,()=>{
+  //     productPageTwo.classList.toggle('show');
+  //   })
+  // })
+  // productPageOneMobileBtn.forEach(item =>{
+  //   item.addEventListener('click' ,()=>{
+  //     productPageThree.classList.toggle('show');
+  //   })
+  // })
+
+
+const buttonBtn = document.querySelector('.button-btn');
+const contentHide = document.querySelector('.content-hidden');
+const btnShow = document.querySelector('.content-hide_btn');
+
+    buttonBtn.addEventListener('click', function(){
+        contentHide.classList.toggle('card');
+        // buttonBtn.style.display = 'none';
+    })
+    btnShow.addEventListener('click', function(){
+        contentHide.classList.toggle('card');
+        // buttonBtn.style.display = 'block';
+    });
+
+
+  });
